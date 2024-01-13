8a31dfaecc7a41083e0d5f6a6c752bf58b22f393
Bestoloc <hinz8amq17x7@mail.ru>
2024-01-13 10:38:56 +0300

    Добавьте файлы проекта.

---
+++ metod/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Step(double A, double K, out double result)
+            {
+                result = 1;
+                for (int i = 1; i <= K; i++)
+                {
+                    result *= A;
+                }
+            }
+            void F(double nn, out double X)
+            {
+                X = 1;
+                for (int i = 1; i <= nn; i++)
+                {
+                    X = X * i;
+                }
+            }
+            Console.Write("n=");
+            double n = double.Parse(Console.ReadLine());
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("y=");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+            Step(x - y, n, out double s1);
+            Step(a - b, n, out double s2);
+            F(n + 3, out double p1);
+            F(n, out double p2);
+
+            double V = (s1 - p1) / (s2 - p2);
+            Console.WriteLine($"V={V}");
+        }
+    }
+}
